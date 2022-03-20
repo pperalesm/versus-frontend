a7cf05ce20f229b9ec8dd33c94a834838d5fb58a
Pablo Perales Molas <pperalesm@gmail.com>
2022-03-20 20:21:54 +0100

    Added login component and formatted all files

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -17,7 +17,7 @@ export class HeaderComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   switchLanguage(change: MatSelectChange) {
     this.translate.use(change.value);
