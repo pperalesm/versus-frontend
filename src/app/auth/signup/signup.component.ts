14c183293cd76d2986076234851c20c34f6a30d0
Pablo Perales Molas <pperalesm@gmail.com>
2022-03-21 21:05:53 +0100

    Added signup component and form fields

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss'],
+})
+export class SignupComponent implements OnInit {
+  signupForm: FormGroup = new FormGroup({
+    usernameFormControl: new FormControl('', [Validators.required]),
+    emailFormControl: new FormControl('', [
+      Validators.required,
+      Validators.email,
+    ]),
+    passwordFormControl: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+  });
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSubmit() {}
+}
