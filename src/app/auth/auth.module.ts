7c7218854a83c9ac1285216b3c5a00403f197aae
Pablo Perales Molas <pperalesm@gmail.com>
2022-03-21 19:36:52 +0100

    Added login component form and fields

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,9 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
+import { MatCardModule } from '@angular/material/card';
+import { TranslateModule } from '@ngx-translate/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    MatCardModule,
+    TranslateModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
+  ],
 })
 export class AuthModule {}
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({
+    emailFormControl: new FormControl('', [Validators.required]),
+    passwordFormControl: new FormControl('', [Validators.required]),
+  });
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSubmit() {}
 }
