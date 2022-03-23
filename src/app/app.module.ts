307a10995303a2fde715ecde42ed5444cbd6998c
Pablo Perales Molas <pperalesm@gmail.com>
2022-03-23 19:36:06 +0100

    Added footer information

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent],
@@ -27,6 +28,7 @@ import { MatSelectModule } from '@angular/material/select';
     }),
     MatButtonModule,
     MatSelectModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { TermsComponent } from '../shared/components/terms/terms.component';
 
 @Component({
   selector: 'app-footer',
@@ -6,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
-  constructor() {}
+  pablo: string = ' Pablo Perales Molas';
+  pabloUrl: string =
+    'https://www.linkedin.com/in/pablo-perales-molas-900723a5/';
+  separator: string = ' · ';
+  version: string = 'v0.0.1';
+
+  constructor(private dialog: MatDialog) {}
 
   ngOnInit(): void {}
+
+  onTermsClicked() {
+    this.dialog.open(TermsComponent);
+  }
 }
