f12348f7734790ca025b98ce3d2e24cd10b090be
Pablo Perales Molas <pperalesm@gmail.com>
2022-06-07 20:06:48 +0200

    Added catalog component

--- src/app/catalog/catalog.module.ts
+++ src/app/catalog/catalog.module.ts
@@ -4,15 +4,18 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { TranslateModule } from '@ngx-translate/core';
 import { CatalogRoutingModule } from './catalog-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CatalogComponent } from './catalog.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [],
+  declarations: [CatalogComponent],
   imports: [
     CatalogRoutingModule,
     SharedModule,
     MatProgressSpinnerModule,
     TranslateModule,
     ReactiveFormsModule,
+    MatCardModule,
   ],
 })
 export class CatalogModule {}
